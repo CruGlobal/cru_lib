6e52016f17d69fca780d9cfa6507138cac9158ad
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-05-04 16:54:55 -0400

    Remove auth code. Moved to cru-auth-lib gem.

--- lib/cru_lib.rb
+++ lib/cru_lib.rb
@@ -5,27 +5,4 @@ require 'cru_lib/global_registry_relationship_methods'
 require 'cru_lib/global_registry_master_person_methods'
 
 module CruLib
-  class << self
-    attr_accessor :redis_host, :redis_port, :redis_db, :redis_client
-
-    def configure
-      yield self
-    end
-
-    def redis_host
-      @redis_host ||= 'localhost'
-    end
-
-    def redis_port
-      @redis_port ||= '6379'
-    end
-
-    def redis_db
-      @redis_db ||= 2
-    end
-
-    def redis_client
-      Redis.new(:host => CruLib.redis_host, :port => CruLib.redis_port, :db => CruLib.redis_db)
-    end
-  end
 end
--- lib/cru_lib/version.rb
+++ lib/cru_lib/version.rb
@@ -1,3 +1,3 @@
 module CruLib
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end
